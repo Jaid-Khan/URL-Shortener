3d2042aba7e95c86b8afdd96332fe4f4e08ddcc4
JAID-KHAN <jaidkhan04072001@gmail.com>
2026-01-06 01:49:20 +0530

    updated package.json run command and port in index.js

--- index.js
+++ index.js
@@ -53,7 +53,9 @@ function middleware(req, res, next) {
 // Apply middleware to unmatched routes
 app.use(middleware);
 
-// Start server on port 3000
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
